8df751dab12e9f99fc932dca6e7671f3f2c0662b
Vincent <qianbo@futsystems.com>
2021-05-04 15:46:31 +0800

    .

--- nas_mgr.py
+++ nas_mgr.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import subprocess
 
 
 class UploadProcess(object):
@@ -18,10 +19,14 @@ class NasManager(object):
     def start_nc(self, plot_name):
         plot_path= '/mnt/dst/00/%s' % plot_name
         df_cmd = 'nc -l -q5 -p 4040 > "%s" < /dev/null' % plot_path
-        os.system(df_cmd)
+        os.spawnl(os.P_DETACH, df_cmd)
 
 
 if __name__ == '__main__':
-    nas = NasManager()
-    nas.start_nc('test_plot_file')
+    df_cmd = 'nc -ld -q5 -p 4040 >/mnt/dst/00/test.file'
+
+    process = subprocess.Popen(df_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    err = process.stderr.read()
+
+
 
