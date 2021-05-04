1568f7b87946f4398741a6fe69b9f75ee0151fe5
Vincent <qianbo@futsystems.com>
2021-05-04 16:11:20 +0800

    .

--- nas_mgr.py
+++ nas_mgr.py
@@ -3,6 +3,8 @@
 
 import os
 import subprocess
+import logging
+logger = logging.getLogger(__name__)
 
 
 class UploadProcess(object):
@@ -17,16 +19,27 @@ class NasManager(object):
 
 
     def start_nc(self, plot_name):
+        """
+        start nc to receive plot file
+        client user command nc 127.0.0.1 4040 -q2 < plot_file_name to send file
+        :param plot_name:
+        :return:
+        """
         plot_path= '/mnt/dst/00/%s' % plot_name
-        df_cmd = 'nc -l -q5 -p 4040 > "%s" < /dev/null' % plot_path
-        os.spawnl(os.P_DETACH, df_cmd)
+        nc_cmd = 'nc -l -q5 -p 4040 > "%s" < /dev/null' % plot_path
+        screen_cmd = "screen -d -m -S nc bash -c '%s'" % nc_cmd
+        logger.info('Nas server start nc to receive plotfile:%s,cmd:%s' % plot_name,screen_cmd)
+        os.spawnl(os.P_DETACH, screen_cmd)
 
 
 if __name__ == '__main__':
-    df_cmd = "screen -d -m -S nc bash -c 'nc -l -q5 -p 4040 >/mnt/dst/00/test.file'"
+    logging.config.fileConfig("logging.conf")
+    logger = logging.getLogger(__name__)
 
-    process = subprocess.Popen(df_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    err = process.stderr.read()
-
+    #df_cmd = "screen -d -m -S nc bash -c 'nc -l -q5 -p 4040 >/mnt/dst/00/test.file'"
+    #process = subprocess.Popen(df_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #err = process.stderr.read()
+    nas = NasManager()
+    nas.start_nc('file1')
 
 
