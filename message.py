b143bb5c8ac0313b92e6f7830a09264c54406e86
Vincent <qianbo@futsystems.com>
2021-05-04 23:16:47 +0800

    add json response

--- message.py
+++ message.py
@@ -1,4 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+
 import json
+
+
+class Response(object):
+    def __init__(self, code, msg, data=None):
+        self._code = code
+        self._msg = msg
+        self._data = data
+
+
+    def to_json(self):
+        return json.dumps(
+            {
+                'code' : self._code,
+                'msg' : self._msg,
+                'data' : self._data
+
+            }
+        )
--- nas_srv.py
+++ nas_srv.py
@@ -3,16 +3,23 @@
 
 
 from flask import Flask
+from flask import request
+from nas_mgr import NasManager
+
 app = Flask(__name__)
+nas = NasManager()
+
 
 @app.route('/')
 def hello_world():
     return 'hello world'
 
-@app.route('/plot/upload')
+@app.route('/plot/upload', methods=['GET', 'POST'])
 def plot_upload():
-    
-    return 'upload file'
+    plot_file = request.form['file']
+    res = nas.start_nc(plot_file)
+
+    return res.to_json()
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
