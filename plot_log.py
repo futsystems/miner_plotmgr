a907d004df6d50ed84cb6653fcc821cab048546e
Vincent <qianbo@futsystems.com>
2021-05-17 16:38:28 +0800

    Update plot_log.py

--- plot_log.py
+++ plot_log.py
@@ -14,15 +14,13 @@ logger = logging.getLogger('nas')
 
 def get_file_list(file_path):
     dir_list = os.listdir(file_path)
-    if not dir_list:
-        return
-    else:
+    if len(dir_list) > 0:
         # 注意，这里使用lambda表达式，将文件按照最后修改时间顺序升序排列
         # os.path.getmtime() 函数是获取文件最后修改时间
         # os.path.getctime() 函数是获取文件最后创建时间
         dir_list = sorted(dir_list,  key=lambda x: x.split('.')[0], reverse=True)
         # print(dir_list)
-        return dir_list
+    return dir_list
 
 def get_plot_statistic():
     import re
