8152d331798b7d8f4db16fd9d24cc5da3ac56f47
Vincent <qianbo@futsystems.com>
2021-05-17 23:57:34 +0800

    Update nas_mgr.py

--- nas_mgr.py
+++ nas_mgr.py
@@ -60,7 +60,8 @@ class NasManager(object):
         #screen_cmd = "screen -d -m -S nc bash -c '%s'" % nc_cmd
         logger.info('Nas server start nc to receive plot file:%s,CMD:%s' % (plot_name, nc_cmd))
         process = subprocess.Popen(nc_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        process.wait()
+        #process.wait() 不能等待否则会导致http request一直没有返回
+        time.sleep(1)
         self.__current_nc = {
             'pid': process.pid,
             'plot_file': plot_name,
