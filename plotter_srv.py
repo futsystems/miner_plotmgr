982be86cab844a0bbe5728b66b014ef702961f4c
Vincent <qianbo@futsystems.com>
2021-05-18 13:22:16 +0800

    Update plotter_srv.py

--- plotter_srv.py
+++ plotter_srv.py
@@ -138,7 +138,7 @@ def config_plotman_ISPLOTTINGRUN():
         if config['code'] == 0:
             logger.info('response:%s plot info data:%s' % (response, config))
             logger.debug('is_plotting_run:%s value:%s' % (config['data']['is_plotting_run'], (1 if config['data']['is_plotting_run'] else 0)))
-            return '%' % (1 if config['data']['is_plotting_run'] else 0)
+            return '%s' % (1 if config['data']['is_plotting_run'] else 0)
         return '0'
     except Exception as e:
         logger.error(traceback.format_exc())
