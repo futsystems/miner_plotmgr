c0767f9c8085bc94ce9a62c4e8b53d7b9546b31d
Vincent <qianbo@futsystems.com>
2021-05-31 15:36:43 +0800

    Update harvester_srv.py

--- harvester_srv.py
+++ harvester_srv.py
@@ -97,7 +97,7 @@ def start_nc():
 
     logger.info('upload plot file:%s' % plot_file)
 
-    res = nas.start_nc(plot_file)
+    res = harvester.start_nc(plot_file)
     return res.to_json()
 
 
@@ -107,7 +107,7 @@ def stop_nc():
     stop nc
     :return:
     """
-    res = nas.stop_nc()
+    res = harvester.stop_nc()
     return res.to_json()
 
 @app.route('/nc/current')
@@ -116,7 +116,7 @@ def current_nc():
     stop nc
     :return:
     """
-    nc = nas.get_current_nc()
+    nc = harvester.get_current_nc()
     return Response(0, '', nc).to_json()
 
 
@@ -129,7 +129,7 @@ def plot_info():
     path = request.args.get('path')
     if path is None or path == '':
         return Response(100, 'path args is empty').to_json()
-    info = nas.get_plot_info(path)
+    info = harvester.get_plot_info(path)
     return Response(0, '', info).to_json()
 
 
