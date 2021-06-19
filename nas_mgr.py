b2100c2521564eda73084b27ce59da744b9ab436
Vincent <qianbo@futsystems.com>
2021-06-19 19:38:18 +0800

    Update nas_mgr.py

--- nas_mgr.py
+++ nas_mgr.py
@@ -193,6 +193,8 @@ class NasManager(object):
         from netifaces import interfaces, ifaddresses, AF_INET
         for ifaceName in interfaces():
             addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr': 'No IP addr'}])]
+            logger.info('interface:%s address:%s' % (ifaceName, addresses))
+            
             if len(addresses) > 1 and addresses[0] != 'No IP addr':
                 if addresses[0].startswith('10.6'):
                     data['biz_interface'] = ifaceName
