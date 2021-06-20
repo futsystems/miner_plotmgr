80d9558e03577283565bd3211caa40cd05871441
Vincent <qianbo@futsystems.com>
2021-06-20 20:58:02 +0800

    Update log_monitor.py

--- log_monitor.py
+++ log_monitor.py
@@ -17,11 +17,13 @@ logger = logging.getLogger('nas')
 class LogMonitor(object):
     def __init__(self, index, log_file):
         self._log_file = log_file
-        self._capicity_value = None
-        self._capicity_unit = 'TB'
-        self._capicity_update_time = datetime.datetime.now()
-        self._capicity_check_time = datetime.datetime.now()
-        self._capicity_check_interval = 1
+        self._capicity_remote_value = None
+        self._capicity_remote_unit = 'TB'
+        self._capicity_remote_update_time = datetime.datetime.now()
+        self._capicity_remote_first_update_time = None
+
+        self._capicity_local_check_time = datetime.datetime.now()
+        self._capicity_local_check_interval = 1
         self._index = index
 
     def start_moniter(self):
@@ -50,9 +52,9 @@ class LogMonitor(object):
     def check_process(self):
         now = datetime.datetime.now()
         # 没有执行过算力检查 或者 距离上次算力检查超过检查间隔
-        if self._capicity_check_time is None or (now-self._capicity_check_time).total_seconds() > self._capicity_check_interval*60:
+        if self._capicity_check_time is None or (now-self._capicity_check_time).total_seconds() > self._capicity_local_check_interval*60:
             # 获得矿池算力值
-            if self._capicity_value is not None:
+            if self._capicity_remote_value is not None:
                 driver_list=[]
                 flag = self._index * 15
                 while flag < (self._index + 1)*15:
@@ -65,10 +67,9 @@ class LogMonitor(object):
 
                 plot_cnt = sum([item['file_cnt'] for item in driver_list])
                 power = round(plot_cnt * 101.4 * 0.0009765625, 2)
-                logger.info('local power:%s remote power:%s %s' % (power, self._capicity_value, self._capicity_unit))
+                logger.info('local power:%s remote power:%s %s' % (power, self._capicity_remote_value, self._capicity_remote_unit))
 
                 self._capicity_check_time = now
-
 
 
     def log_process(self, log_line):
@@ -78,22 +79,18 @@ class LogMonitor(object):
             time_value = log_line[6:25]
             dt = datetime.datetime.fromisoformat(time_value)
             if (now - dt).total_seconds() < 120:
-                logger.debug('event time:%s passed in 2 minutes' % time_value)
-                logger.debug('====> %s' % log_line)
+                #logger.debug('event time:%s passed in 2 minutes' % time_value)
+                #logger.debug('====> %s' % log_line)
                 if 'capacity' in log_line:
                     tmp = log_line.split('capacity="')
                     capacity_data = tmp[1].split('"')[0]
                     items = capacity_data.split(' ')
                     logger.info('capicity data:%s' % capacity_data)
-                    self._capicity_value = float(items[0])
-                    self._capicity_unit = items[1]
-                    self._capicity_update_time = dt
-
-
-
-
-
-
+                    if self._capicity_remote_value is None:
+                        self._capicity_remote_first_update_time = datetime.datetime.now()
+                    self._capicity_remote_value = float(items[0])
+                    self._capicity_remote_unit = items[1]
+                    self._capicity_remote_update_time = dt
 
                 #logger.info('check data:%s' % items[3])
                 #tmp_data = items[3].split('=')
