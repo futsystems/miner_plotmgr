ccaa3f31ff156f33ceb7145472b6d50b4507cbb1
Vincent <qianbo@futsystems.com>
2021-06-20 14:54:29 +0800

    Update log_monitor.py

--- log_monitor.py
+++ log_monitor.py
@@ -46,8 +46,8 @@ class LogMonitor(object):
             data = item.split('=')
             if len(data) == 2:
                 if data[0] == 'time':
-                    dt = datetime.datetime.strptime(data[1], '%Y-%m-%dT%H:%M:%SZ')
-                    if (now - dt).total_seconds() > 120:
+                    dt = datetime.datetime.fromisoformat(data[1][:-1])
+                    if (now - dt).total_seconds() < 120:
                         logger.info('in 2 minuts')
                         logger.info(log_line)
 
